c5eb26158352da499a3d6d6509c5230436274e7f
SavorTheFlavor <2500364530@qq.com>
2018-12-24 21:34:56 +0800

    ...

--- Scala/learning/Life.scala
+++ Scala/learning/Life.scala
@@ -8,11 +8,15 @@ class Life {
   var success: Boolean = _
   val maxAge = 100
   // private[this] means it is the private of 'this' object, private[spark], private[person]
+  // e.g. 对于对象life1和life2，如果只是private的话，life1里面是可以访问到life2的secret
   private[this] var secret: String = _
   private var secret2: String = _
 
+  def test(life: Life): Unit = {
+     //print(life.secret) //compile error, because of private[this]
+  }
+  
   def live(): Unit = {
-
   }
 
   def die(): Unit = {
