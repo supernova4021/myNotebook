9b3365d57bb346945b25c1c423d9f7d14ec8eb82
SavorTheFlavor <2500364530@qq.com>
2019-03-19 18:44:31 +0800

    three way quick sort

--- algorithm/Sort/quickSort.cpp
+++ algorithm/Sort/quickSort.cpp
@@ -28,3 +28,38 @@ int partition(vector<int> arr, int low, int high){
 	arr[low]=p;
 	return low;
 }
+
+// three way quick sort
+public static void qsort(int[] a){  
+	//为获得接近于最好的排序性能，先打乱再排序  
+	shuffle(a);  
+	qsort(a,0,a.length - 1);  
+}  
+  
+/** 
+ * 三向切分的快速排序实现 
+ * @param a 
+ * @param lo 
+ * @param hi 
+ */  
+private static void qsort(int[] a,int lo,int hi){ 
+	if(lo < hi){
+		int lt = lo;
+		int i = lo + 1;
+		int gt = hi;
+		int pivot = a[lo];
+		while(i <= gt){
+			if(a[i] < pivot){
+				swap(a, lt++, i++);
+			}else if(a[i] > pivot){
+				//这里i不能--，因为交换后i的位置还未确定
+				//上面的lt可以++是因为lt指向的是还未确定的
+				swap(a, i, gt--);
+			}else{
+				i++;
+			}
+		}
+		qsort(a, lo, lt - 1);
+		qsort(a, gt + 1, hi);
+	}
+}
