d9bea80e9c7bd55f7be6c0ab2b37f445c6be1d32
SavorTheFlavor <2500364530@qq.com>
2020-03-04 12:32:08 +0800

    bugfix

--- DataStructure/MyUnionFind.java
+++ DataStructure/MyUnionFind.java
@@ -26,6 +26,7 @@ public class MyUnionFind {
         // Weighted Quick-Union
         // always link the root of smaller tree to the root of larger tree
         if(size[r2] < size[r1]){
+            // set r2's parent to r1
             ids[r2] = r1;
             size[r1] += size[r2];
         }else{
